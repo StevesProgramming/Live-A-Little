4280cff0ac340d35e79e41d7cb25c939b025fa21
Steven Finn <stevenfinn.flp@gmail.com>
2023-02-20 19:56:09 +0000

    General changes across files in order to get app to boot successfully. Mostly name changes and typos

--- app/src/main/java/com/example/live_a_little/MainActivity.kt
+++ app/src/main/java/com/example/live_a_little/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.live_a_little
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLogin: Button
+    private lateinit var txtSignup: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.loginsignup_login_button)
+        txtSignup = findViewById(R.id.loginsignup_signup_text)
+
+        txtSignup.setOnClickListener {
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+        }
+
+//            btnLogin.setOnClickListener{
+//                val intent = Intent(this, Login::class.java)
+//                startActivity(intent)
+//            }
     }
 }
