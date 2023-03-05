6e26eca64a2ae717b322067d1200f4cb27c2afaa
Steven Finn <stevenfinn.flp@gmail.com>
2023-03-05 16:48:55 +0000

    Added top navigation menu to the achievement screen and Adding correct naming

---
+++ app/src/main/java/com/example/live_a_little/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.live_a_little
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
