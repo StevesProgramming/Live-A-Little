abdc25ade11d238120987a16db9dd4a3e8b72bfd
Steven Finn <stevenfinn.flp@gmail.com>
2023-03-05 16:22:23 +0000

    Adding navigation to the bottom of the screen. Adding Blank Home page with navigation too

---
+++ app/src/main/java/com/example/live_a_little/Home.kt
@@ -0,0 +1,11 @@
+package com.example.live_a_little
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
