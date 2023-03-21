ddcfc760c485747698543527f22ad3c0a4bd199f
Steven Finn <stevenfinn.flp@gmail.com>
2023-03-21 16:16:39 +0000

    Clearning up code

--- app/src/main/java/com/example/live_a_little/signup.kt
+++ app/src/main/java/com/example/live_a_little/signup.kt
@@ -1,14 +1,18 @@
 package com.example.live_a_little
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Signup : AppCompatActivity() {
     private lateinit var etUsername: EditText
@@ -16,17 +20,17 @@ class Signup : AppCompatActivity() {
     private lateinit var etConfirmEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var etConfirmPassword: EditText
-
     private lateinit var btnSignup: Button
     private lateinit var btnBack: Button
-    private lateinit var dbRef: DatabaseReference
-
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var Firestore: FirebaseFirestore
 
-
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        Firestore = FirebaseFirestore.getInstance()
 
         etUsername = findViewById(R.id.usernameInput)
         etEmail = findViewById(R.id.emailInput)
@@ -36,8 +40,6 @@ class Signup : AppCompatActivity() {
 
         btnSignup = findViewById(R.id.btnLogout)
         btnBack= findViewById(R.id.signup_back_button)
-        dbRef = FirebaseDatabase.getInstance("https://project-cw-34e62-default-rtdb.europe-west1.firebasedatabase.app/")
-            .getReference("users")
 
         btnBack.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -72,11 +74,31 @@ class Signup : AppCompatActivity() {
         } else {
             if(password == confirmPassword && email == confirmEmail){
                 firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                    if(it.isSuccessful){
-                        Toast.makeText(this, "Signup Successful", Toast.LENGTH_LONG).show()
+                    val user_id = firebaseAuth.uid.toString();
+                    val user_name = firebaseAuth.currentUser?.displayName.toString()
+                    val user_email = firebaseAuth.currentUser?.email.toString()
+                    val user_details = hashMapOf(
+                        username to user_name,
+                        email to user_email
+                    )
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
+                    if(it.isSuccessful){
+                        db.collection("users").document(user_id)
+                            .set(user_details)
+                            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                        db.collection("achievements")
+                            .get()
+                            .addOnSuccessListener { achievement_documents ->
+                                for (document in achievement_documents) {
+                                    db.collection("users").document(user_id)
+                                        .collection("user_achievements")
+                                        .add(document)
+                                }
+                            }
+                        Toast.makeText(this, "Signup Successful", Toast.LENGTH_LONG).show()
+                        openLogin()
                     }
                     else{
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
@@ -90,5 +112,13 @@ class Signup : AppCompatActivity() {
                 etConfirmEmail.error = "Emails do not match"
             }
         }
+    }
+    private fun makeUserRecord(etUsername: String, etEmail: String){
+        data class User(val username: String, val email: String)
+        val userData = User(etUsername, etEmail)
+    }
+    private fun openLogin(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
