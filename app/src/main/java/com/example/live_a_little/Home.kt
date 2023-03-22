bd17b1e76f0d756c98609dcdeae9e90542605c46
Steven Finn <stevenfinn.flp@gmail.com>
2023-03-22 17:53:50 +0000

    Adding firebase auth to home screen

--- app/src/main/java/com/example/live_a_little/Home.kt
+++ app/src/main/java/com/example/live_a_little/Home.kt
@@ -3,16 +3,21 @@ package com.example.live_a_little
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
 
 class Home : AppCompatActivity() {
     private lateinit var btnAchievement: ImageButton
     private lateinit var btnProfile: ImageButton
+    private lateinit var firebaseAuth: FirebaseAuth
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        firebaseAuth = FirebaseAuth.getInstance()
         btnAchievement= findViewById(R.id.achievements_button)
         btnProfile= findViewById(R.id.profile_button)
 
