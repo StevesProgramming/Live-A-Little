49e10c72deb0aa3783fa66647d51ddf02095a962
Steven Finn <stevenfinn.flp@gmail.com>
2023-03-22 17:53:15 +0000

    Adding friends list to profile

--- app/src/main/java/com/example/live_a_little/Profile.kt
+++ app/src/main/java/com/example/live_a_little/Profile.kt
@@ -3,19 +3,26 @@ package com.example.live_a_little
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
 
     private lateinit var btnHome: ImageButton
     private lateinit var btnAchievement: ImageButton
+    private lateinit var btnLogout: Button
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        firebaseAuth = FirebaseAuth.getInstance()
         btnHome= findViewById(R.id.home_button)
         btnAchievement= findViewById(R.id.achievements_button)
+        btnLogout= findViewById(R.id.btnLogout)
 
         btnHome.setOnClickListener{
             openHome()
@@ -23,6 +30,15 @@ class Profile : AppCompatActivity() {
 
         btnAchievement.setOnClickListener{
             openAchievements()
+        }
+
+        btnAchievement.setOnClickListener{
+            openAchievements()
+        }
+
+        btnLogout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut();
+            openLogin()
         }
     }
 
@@ -33,6 +49,11 @@ class Profile : AppCompatActivity() {
 
     private fun openAchievements(){
         val intent = Intent(this, Achievements::class.java)
+        startActivity(intent)
+    }
+
+    private fun openLogin(){
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
