2619f0941d8bbf8a22519bb1ee0900458048daa1
Steven Finn <stevenfinn.flp@gmail.com>
2023-04-14 16:58:18 +0100

    Home page layout working

--- app/src/main/java/com/example/live_a_little/Home.kt
+++ app/src/main/java/com/example/live_a_little/Home.kt
@@ -1,42 +1,175 @@
 package com.example.live_a_little
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Home : AppCompatActivity() {
     private lateinit var btnAchievement: ImageButton
     private lateinit var btnProfile: ImageButton
+
+    private var usernameList = ArrayList<String>()
+    //private var achievementNameList = ArrayList<String>()
+    private val achievements: MutableMap<Any, ArrayList<String>> = HashMap()
+
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: HomeAdapter
+    private lateinit var achievementDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        btnAchievement= findViewById(R.id.achievements_button)
-        btnProfile= findViewById(R.id.profile_button)
+        btnAchievement = findViewById(R.id.achievements_button)
+        btnProfile = findViewById(R.id.profile_button)
 
-        btnAchievement.setOnClickListener{
+        populateHome()
+
+        btnAchievement.setOnClickListener {
             openAchievements()
         }
 
-        btnProfile.setOnClickListener{
+        btnProfile.setOnClickListener {
             openProfile()
         }
     }
 
-    private fun openAchievements(){
+    private fun openAchievements() {
         val intent = Intent(this, Achievements::class.java)
         startActivity(intent)
     }
 
-    private fun openProfile(){
+    private fun openProfile() {
         val intent = Intent(this, Profile::class.java)
         startActivity(intent)
     }
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun populateHome(){
+        val db = Firebase.firestore
+        val user_id = firebaseAuth.uid.toString();
+
+        val friends = db.collection("users").document(user_id)
+            .collection("friends")
+
+        usernameList.clear()
+        //achievementNameList.clear()
+        achievements.clear()
+
+        // Initialise recycle view and adapter
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this@Home)
+
+        adapter = HomeAdapter(usernameList, achievements,this@Home)
+        recyclerView.adapter = adapter
+
+        // TODO: Switch to a dictionary key-value pair storage
+        /*  Reason: When having 2 lists I can loop through the lists
+            But there is no way of telling which achievements belong to which user
+            therefore having a dictionary would allow me to look at each key and output
+            each value associated with that key
+        */
+
+
+
+        friends
+            .get()
+            .addOnCompleteListener { friends_documents ->
+                if (friends_documents.isSuccessful) {
+                    for (friendDocument in friends_documents.result) {
+                        val userData = UserModel(friendDocument)
+                        val userId = userData.userId
+                        val username = userData.username
+
+                        usernameList.add(username)
+
+                        val users = db.collection("users").document(userId)
+                            .collection("user_achievements")
+
+                        users
+                            .whereEqualTo("data.complete", true)
+                            .get()
+                            .addOnCompleteListener { users_documents ->
+                                val achievementNameList = mutableListOf<String>()
+
+                                for (userDocument in users_documents.result) {
+                                    val achievement_data = AchievementsModel(userDocument)
+                                    val achievementName = achievement_data.name
+
+                                    Log.d("Testing", achievementName)
+                                    Log.d("Testing", achievement_data.toString())
+
+                                    achievementNameList.add(achievementName)
+                                }
+
+//                                for(achievementName in  achievementNameList){
+//                                    achievements[username].add(achievementName)
+//                                }
+                                achievements[username] = achievementNameList as ArrayList<String>
+
+                                Log.d("Testing 1", "1" + achievements[username].toString())
+                                adapter.notifyDataSetChanged()
+                            }
+                    }
+                    Log.d("Testing", usernameList.toString())
+                    //Log.d("Testing", achievementNameList.toString())
+                }
+
+            }
+
+
+
+
+
+
+
+
+//        achievements
+//            .get()
+//            .addOnCompleteListener { achievement_documents ->
+//                if (achievement_documents.isSuccessful) {
+//                    for (document in achievement_documents.result) {
+//                        val achievement_data = AchievementsModel(document)
+//
+//                        val username = achievement_data.name
+//                        val achievementName = achievement_data.name
+
+
+//                        if (achievementName != null && achievementDesc != null) {
+//                            if(currentList == "incomplete" && achievementComplete == false){
+//                                achievementIDList.add(achievementId)
+//                                achievementNameList.add(achievementName)
+//                                achievementDescList.add(achievementDesc)
+//                                achievementCompleteList.add(achievementComplete)
+//                                achievementGoalList.add(achievementGoal)
+//                                achievementSuccessfulCompletionList.add(achievementSuccessfulCompletion)
+//                            }
+//                            else if(currentList == "complete" && achievementComplete == true){
+//                                achievementIDList.add(achievementId)
+//                                achievementNameList.add(achievementName)
+//                                achievementDescList.add(achievementDesc)
+//                                achievementCompleteList.add(achievementComplete)
+//                                achievementGoalList.add(achievementGoal)
+//                                achievementSuccessfulCompletionList.add(achievementSuccessfulCompletion)
+//                            }
+//                        }
+//                    }
+//                    adapter.notifyDataSetChanged()
+                }
+//                else{
+//                    Log.d(ContentValues.TAG, "Error getting documents: ", achievement_documents.exception)
+//                }
+//            }
 }
--- app/src/main/java/com/example/live_a_little/HomeAdapter.kt
+++ app/src/main/java/com/example/live_a_little/HomeAdapter.kt
@@ -3,242 +3,199 @@ package com.example.live_a_little
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.PopupWindow
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatImageButton
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
-class AchievementsAdapter(
-    var nameList: ArrayList<String>,
-    var descList: ArrayList<String>,
-    var completeList: ArrayList<Boolean>,
-    var goalList: ArrayList<Int>,
-    var successfullyCompleteList: ArrayList<Int>,
-    var achievementIDList: ArrayList<String>,
-    var context: Context) : RecyclerView.Adapter<AchievementsAdapter.AchievementViewHolder>() {
+class HomeAdapter(
+    var usernameList: ArrayList<String>,
+    //var achievementNameList: ArrayList<String>,
+    var achievements: MutableMap<Any, ArrayList<String>>,
+    var context: Context) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
 
     private lateinit var firebaseAuth: FirebaseAuth
-    var adapter: AchievementsAdapter? = null
+    var adapter: HomeAdapter? = null
 
-    class AchievementViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textViewTitle: TextView = itemView.findViewById(R.id.textViewTitle)
-        var textViewDesc: TextView = itemView.findViewById(R.id.textViewDesc)
-        var cardView: CardView = itemView.findViewById(R.id.cardView)
+        var textViewAchievementTitle: TextView = itemView.findViewById(R.id.textViewAchievementTitle)
+        val titleCardView: CardView = itemView.findViewById(R.id.titleCardView)
+        //val designLayout: CardView = itemView.findViewById(R.id.designLayout)
+        val achievementCardView: CardView = itemView.findViewById(R.id.achievementCardView)
+        val homeCardDesign: LinearLayout = itemView.findViewById(R.id.homeCardDesign)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AchievementViewHolder {
-        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.card_design
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.home_card_design
             , parent
             , false)
-        return AchievementViewHolder(view)
+        return HomeViewHolder(view)
     }
 
     @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(holder: AchievementViewHolder, position: Int) {
-        holder.textViewTitle.text = nameList[position]
-        holder.textViewDesc.text = descList[position]
-        holder.cardView.setOnClickListener {
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
 
-            val achievementID = achievementIDList[position]
-            val goal = goalList[position]
-            val complete = completeList[position]
-            val successfullyComplete = successfullyCompleteList[position]
+        val username = usernameList[position]
+        val achievementNames = achievements[username]
 
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val popupView: View
+        holder.homeCardDesign.removeAllViews()
 
-            val popupWindowType: String
+        //val linearLayout = LinearLayout(context)
+        //linearLayout.orientation = LinearLayout.VERTICAL
 
-            // deciding which popup to display based on whether the achievement
-            // is an incremental achievement or single task achievement
-            if(complete == false){
-                if(goal > 1 && successfullyComplete != goal){
-                    popupView = inflater.inflate(R.layout.popup_window_increment, null)
-                    popupWindowType = "popup_window_increment"
-                } else{
-                    popupView = inflater.inflate(R.layout.popup_window, null)
-                    popupWindowType = "popup_window"
+        if (holder.titleCardView.childCount == 1) {
+            val titleCardView = createTitleCard(username)
+            holder.homeCardDesign.addView(titleCardView)
+        }
+
+        val layout = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layout.setMargins(0, 10, 17, 10)
+        layout.gravity = Gravity.END
+
+        val achievementCardView = LinearLayout(context)
+        achievementCardView.orientation = LinearLayout.VERTICAL
+
+        // Apply the layout parameters to the achievement card view
+        //achievementCardView.layoutParams = layout
+
+        holder.homeCardDesign.addView(achievementCardView)
+
+        if (achievementNames != null) {
+            for (achievementName in achievementNames) {
+                val achievementCard = createAchievementCard(achievementName)
+                if (achievementCard != null) {
+                    achievementCard.layoutParams = layout
                 }
-            }
-            else{
-                popupView = inflater.inflate(R.layout.popup_window_remove, null)
-                popupWindowType = "popup_window_remove"
-            }
-
-            // Designing the display of the popup window
-            val width = LinearLayout.LayoutParams.MATCH_PARENT
-            val height = LinearLayout.LayoutParams.MATCH_PARENT
-            val focusable = true // let user click outside to dismiss
-            val popupWindow = PopupWindow(popupView, width, height, focusable)
-
-            // displaying the popup window on screen
-            popupWindow.showAtLocation(holder.cardView, Gravity.CENTER, 0, 0)
-            popupWindow.setFocusable(false);
-
-            // set up the title and description in popup
-            val textViewTitle = popupView.findViewById<TextView>(R.id.textViewTitle)
-            val textViewDesc = popupView.findViewById<TextView>(R.id.textViewDesc)
-            textViewTitle.text = nameList[position]
-            textViewDesc.text = descList[position]
-
-            // Popup logic
-            if(popupWindowType == "popup_window_increment"){
-                val btnIncrementPlus = popupView.findViewById<AppCompatButton>(R.id.btnIncrementPlus)
-                val btnIncrementMinus = popupView.findViewById<AppCompatButton>(R.id.btnIncrementMinus)
-                val btnIncrement = popupView.findViewById<AppCompatButton>(R.id.btnIncrement)
-
-                btnIncrement.text = "$successfullyComplete/$goal Complete"
-
-                btnIncrementPlus.setOnClickListener {
-                    Log.d("Test: ", "btnIncrementPlus button clicked")
-                    val task = "increase"
-                    increaseOrDecreaseSuccessfullyCompletions(achievementID, task, successfullyComplete)
-                }
-
-                btnIncrementMinus.setOnClickListener {
-                    Log.d("Test: ", "btnIncremementMinus button clicked")
-                    val task = "decrease"
-                    increaseOrDecreaseSuccessfullyCompletions(achievementID, task, successfullyComplete)
-                }
-            }
-            else if(popupWindowType == "popup_window"){
-                val btnComplete = popupView.findViewById<AppCompatButton>(R.id.btnComplete)
-
-                btnComplete.setOnClickListener {
-                    Log.d("Test: ", "btnComplete button clicked")
-                    markAchievementComplete(achievementID, successfullyComplete, goal)
-
-                }
-            }
-            else if(popupWindowType == "popup_window_remove"){
-                val btnRemove = popupView.findViewById<AppCompatButton>(R.id.btnRemove)
-
-                btnRemove.setOnClickListener {
-                    Log.d("Test: ", "btnRemove button clicked")
-                    popupWindow.dismiss()
-
-                    val confirmInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-                    val confirmPopupView = inflater.inflate(R.layout.popup_remove_confirmation, null)
-
-                    // Designing the display of the popup window
-                    val confirmPopupWindow = PopupWindow(confirmPopupView, width, height, focusable)
-
-                    // displaying the popup window on screen
-                    confirmPopupWindow.showAtLocation(holder.cardView, Gravity.CENTER, 0, 0)
-                    confirmPopupWindow.setFocusable(false);
-
-                    val btnNo = confirmPopupView.findViewById<AppCompatButton>(R.id.btnRemoveAchievementNo)
-                    val btnYes = confirmPopupView.findViewById<AppCompatButton>(R.id.btnRemoveAchievementYes)
-
-                    btnNo.setOnClickListener{
-                        confirmPopupWindow.dismiss()
-                    }
-
-                    btnYes.setOnClickListener{
-                        Log.d("Test: ", "Yes button clicked")
-                        markAchievementIncomplete(achievementID, successfullyComplete)
-                        confirmPopupWindow.dismiss()
-                    }
-                }
-            }
-
-            // enable close button
-            val closeButton = popupView.findViewById<AppCompatImageButton>(R.id.btnClose)
-            closeButton.setOnClickListener {
-                popupWindow.dismiss()
+                achievementCardView.addView(achievementCard)
             }
         }
+
+
+//        for ((username, achievement) in achievements) {
+//            println("Key: $username")
+//            println("Values: $achievement")
+//            for (value in achievement) {
+//                println(value)
+//                val achievementCardView = createAchievementCard(value)
+//                holder.achievementCardView.addView(achievementCardView)
+//            }
+//        }
+
+
+
+
+//        if (achievementName != null) {
+//            for (achievement in achievementName) {
+//                val achievementCardView = createAchievementCard(achievement)
+//                holder.achievementCardView.addView(achievementCardView)
+//            }
+//
+//        }
+
+//        achievements.forEach { achievement ->
+//            val achievementCardView = createAchievementCard(achievement.toString())
+//            holder.achievementCardView.addView(achievementCardView)
+//        }
+
+
+
+
+
+//        if(usernameList != null) {
+//            for(user in usernameList){
+//
+//                holder.titleCardView.visibility = View.VISIBLE
+//                holder.textViewTitle.text = username
+//
+//                if (achievementName != null) {
+//                    for (achievement in achievementName) {
+//                        // Inflate the layout for the achievement
+//                        val achievementLayout = LayoutInflater.from(context)
+//                            .inflate(R.layout.home_card_design, holder.achievementCardView, false)
+//
+//                        // Set the text and image for the achievement layout
+//                        achievementLayout.findViewById<TextView>(R.id.textViewAchievementTitle)
+//                            .text = achievement
+//                        achievementLayout.findViewById<ImageView>(R.id.imageView3)
+//                            .setImageResource(R.drawable.logo)
+//
+//                        // Add the achievement layout to the designLayout
+//                        holder.achievementCardView.addView(achievementLayout)
+//                    }
+//                }
+//            }
+//
+//        }
+
+
+
+
+//        Log.d("Testing", "2" + achievements.toString())
+//
+//        val previousName = ""
+//        for((user, achievement) in achievements){
+//            holder.titleCardView.visibility = View.VISIBLE
+//            holder.textViewTitle.text = user.toString()
+//
+//            val list = achievements[user]
+//
+//            if (list != null) {
+//                for(achievment in list){
+//                    holder.textViewAchievementTitle.text = achievment
+//                }
+//            }
+//        }
+
+//        if (position == 0 || achievementNameList[position] != achievementNameList[position-1]) {
+//            // This is the first achievement in the list or a new achievement, so show the titleCardView
+//            holder.titleCardView.visibility = View.VISIBLE
+//            holder.textViewTitle.text = usernameList[position]
+//            //holder.textViewTitleDate.text = currentDate
+//        } else {
+//            // This is not the first achievement in the list and belongs to the same achievement as the previous one, so hide the titleCardView
+//            holder.titleCardView.visibility = View.GONE
+//        }
+//
+//        //holder.textViewTitle.text = usernameList[position]
+//        holder.textViewAchievementTitle.text = achievementNameList[position]
     }
 
     override fun getItemCount(): Int {
-        return nameList.size
+        return usernameList.size
     }
 
-    fun markAchievementComplete(achievementID: Any?, successfullyComplete: Int?, goal: Int?) {
-        firebaseAuth = FirebaseAuth.getInstance()
-        val db = Firebase.firestore
-        val user_id = firebaseAuth.uid.toString();
-        val achievementID = achievementID as String
+    private fun createTitleCard(username: String): View? {
+        //val view = LayoutInflater.from(context).inflate(R.layout.home_card_design, null)
+        val view = LayoutInflater.from(context).inflate(R.layout.home_title, null)
 
-        val achievements = db.collection("users").document(user_id)
-            .collection("user_achievements").document(achievementID)
+        Log.d("Username",  username)
+        val titleTextView = view.findViewById<TextView>(R.id.textViewTitle)
+        titleTextView.text = username
 
-        if (successfullyComplete != null) {
-            if(successfullyComplete < goal!!){
-                val successfullyComplete = successfullyComplete.plus(1)
-                val update = hashMapOf<String, Any>(
-                    "data.complete" to true,
-                    "data.successful_completions" to (successfullyComplete.toInt() ?: 0)
-                )
-                achievements.update(update)
-            }
-            else{
-                val update = hashMapOf<String, Any>(
-                    "data.complete" to true
-                )
-                achievements.update(update)
-            }
-        }
+        return view
     }
 
-    fun markAchievementIncomplete(achievementID: Any?, successfullyComplete: Int?) {
-        firebaseAuth = FirebaseAuth.getInstance()
-        val db = Firebase.firestore
-        val user_id = firebaseAuth.uid.toString();
-        val achievementID = achievementID as String
-        val successfullyComplete = successfullyComplete?.minus(1)
+    private fun createAchievementCard(achievement: String): View? {
+        //val view = LayoutInflater.from(context).inflate(R.layout.home_card_design, null)
+        val view = LayoutInflater.from(context).inflate(R.layout.home_achievement, null)
 
-        val achievements = db.collection("users").document(user_id)
-            .collection("user_achievements").document(achievementID)
+        val achievementTitleTextView = view.findViewById<TextView>(R.id.textViewAchievementTitle)
+        achievementTitleTextView.text = achievement
 
-        val update = hashMapOf<String, Any>(
-            "data.complete" to false,
-            "data.successful_completions" to (successfullyComplete?.toInt() ?: 0)
-        )
-        achievements.update(update)
+        return view
     }
 
-    fun increaseOrDecreaseSuccessfullyCompletions(achievementID: Any?, task: Any?, successfullyComplete: Int?) {
-        firebaseAuth = FirebaseAuth.getInstance()
-        val db = Firebase.firestore
-        val user_id = firebaseAuth.uid.toString();
-        val achievementID = achievementID as String
-
-        val achievements = db.collection("users").document(user_id)
-            .collection("user_achievements").document(achievementID)
-
-        if(task == "increase"){
-            val hashmap = hashMapOf<String, Int>()
-            val successfullyComplete = successfullyComplete?.plus(1)
-
-            val update = hashMapOf<String, Any>(
-                "data.successful_completions" to (successfullyComplete?.toInt() ?: 0)
-            )
-            achievements.update(update)
-        }
-
-        else if(task == "decrease"){
-            val hashmap = hashMapOf<String, Int>()
-            if (successfullyComplete != null) {
-                if(successfullyComplete > 0){
-                    val successfullyComplete = successfullyComplete?.minus(1)
-                    val update = hashMapOf<String, Any>(
-                        "data.successful_completions" to (successfullyComplete?.toInt() ?: 0)
-                    )
-                    achievements.update(update)
-                }
-            }
-        }
-    }
 }
